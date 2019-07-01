9eaf2632f13a8692030191efe58d0e0096a80b4e
Dustin Boling <dustin@dustinboling.com>
2019-07-01 15:59:04 -0700

    wip favicon, etc.

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,16 +16,46 @@ export default {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#da532c'
+      }
+
+      {
+        name: 'theme-color',
+        content: '#ffffff'
       }
     ],
     link: [{
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon.-32x32.png'
+      },
+      {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico'
+        sizes: '16x16',
+        href: '/favicon-16x16.png'
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest"
       },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900&display=swap'
+      },
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#5bbad5"
       }
     ]
   },
