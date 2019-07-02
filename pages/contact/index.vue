0751c16de32b925ce50ba6dcddc586c4a2a81eb1
Dustin Boling <dustin@dustinboling.com>
2019-07-02 01:26:10 -0700

    adds Netlify form and success page

---
+++ pages/contact/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+    <div class="container mx-auto">
+      <form
+        name="contact"
+        method="POST"
+        action="/contact/success"
+        data-netlify="true"
+        data-netlify-recaptcha="true"
+      >
+        <p>
+          <label>
+            Your Name:
+            <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email:
+            <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Role:
+            <select name="role[]" multiple>
+              <option value="leader">Leader</option>
+              <option value="follower">Follower</option>
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <textarea name="message"></textarea>
+          </label>
+        </p>
+        <div data-netlify-recaptcha="true"></div>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+	@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
---
+++ pages/contact/success.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div class="container mx-auto">
+      <h1>Success!</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+	@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
