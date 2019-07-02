b6831b4fa48b394c50e5332ccdbb67950ecfdd09
Dustin Boling <dustin@dustinboling.com>
2019-07-01 17:05:29 -0700

    adds bizz-buzz orange

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        bborange: {
+          lighter: '#FFC226',
+          default: '#FFC226',
+          darker: '#FF8D00'
+        }
+      }
+    }
   },
   variants: {}
 }
