1a66e30eff6961f0ce945c89f3cd53ff2062b29e
Dustin Boling <dustin@dustinboling.com>
2019-07-02 14:02:56 -0700

    adds Indie Flower font from Google

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,95 +1,99 @@
 export default {
-  mode: 'universal',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: 'Bizz-Buzz Media | Exclusive Local Business Networking & Digital Marketing',
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      },
-      {
-        name: 'msapplication-TileColor',
-        content: '#da532c'
-      },
-      {
-        name: 'theme-color',
-        content: '#ffffff'
-      }
-    ],
-    link: [{
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        href: '/favicon-32x32.png'
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        href: '/favicon-16x16.png'
-      },
-      {
-        rel: "manifest",
-        href: "/site.webmanifest"
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900&display=swap'
-      },
-      {
-        rel: 'mask-icon',
-        href: '/safari-pinned-tab.svg',
-        color: '#5bbad5'
-      }
-    ]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: {
-    color: '#fff'
-  },
-  /*
-   ** Global CSS
-   */
-  css: [
-    '~/assets/css/tailwind.css'
-  ],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [],
-  /*
-   ** Build configuration
-   */
-  build: {
-    postcss: {
-      plugins: {
-        tailwindcss: './tailwind.config.js'
-      }
+    mode: 'universal',
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: 'Bizz-Buzz Media | Exclusive Local Business Networking & Digital Marketing',
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            },
+            {
+                name: 'msapplication-TileColor',
+                content: '#da532c'
+            },
+            {
+                name: 'theme-color',
+                content: '#ffffff'
+            }
+        ],
+        link: [{
+                rel: 'apple-touch-icon',
+                sizes: '180x180',
+                href: '/apple-touch-icon.png'
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '32x32',
+                href: '/favicon-32x32.png'
+            },
+            {
+                rel: 'icon',
+                type: 'image/png',
+                sizes: '16x16',
+                href: '/favicon-16x16.png'
+            },
+            {
+                rel: "manifest",
+                href: "/site.webmanifest"
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,900&display=swap'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Indie+Flower&display=swap'
+            },
+            {
+                rel: 'mask-icon',
+                href: '/safari-pinned-tab.svg',
+                color: '#5bbad5'
+            }
+        ]
     },
     /*
-     ** You can extend webpack config here
+     ** Customize the progress-bar color
      */
-    extend(config, ctx) {}
-  }
+    loading: {
+        color: '#fff'
+    },
+    /*
+     ** Global CSS
+     */
+    css: [
+        '~/assets/css/tailwind.css'
+    ],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+    /*
+     ** Build configuration
+     */
+    build: {
+        postcss: {
+            plugins: {
+                tailwindcss: './tailwind.config.js'
+            }
+        },
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
-  theme: {
-    extend: {
-      colors: {
-        bborange: {
-          lighter: '#FFC226',
-          default: '#FFC226',
-          darker: '#FF8D00'
+    theme: {
+        extend: {
+            colors: {
+                bborange: {
+                    lighter: '#FFC226',
+                    default: '#FFC226',
+                    darker: '#FF8D00'
+                }
+            },
+            fontFamily: {
+                'hand': ['Indie\\ Flower', 'cursive']
+            }
         }
-      }
-    }
-  },
-  variants: {}
+    },
+    variants: {}
 }
